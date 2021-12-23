55865d2b235d9a413b5db9f42cefeab3d768fa9d
Alessandro Crugnola <alessandro.crugnola@gmail.com>
2021-12-23 10:37:49 +0100

    adding tests

--- RxJavaExtensions/src/androidTest/java/it/sephiroth/android/rxjava3/extensions/FlowableAndroidTest.kt
+++ RxJavaExtensions/src/androidTest/java/it/sephiroth/android/rxjava3/extensions/FlowableAndroidTest.kt
@@ -1,18 +1,22 @@
 package it.sephiroth.android.rxjava3.extensions
 
+import android.os.Looper
 import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
 import io.reactivex.rxjava3.core.BackpressureStrategy
+import io.reactivex.rxjava3.core.Flowable
 import io.reactivex.rxjava3.core.Observable
+import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
-import it.sephiroth.android.rxjava3.extensions.flowable.pingPong
-import it.sephiroth.android.rxjava3.extensions.flowable.skipBetween
+import it.sephiroth.android.rxjava3.extensions.flowable.*
+import it.sephiroth.android.rxjava3.extensions.observers.AutoDisposableSubscriber
 import org.junit.Assert
 import org.junit.Test
 import org.junit.runner.RunWith
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicReference
 
 
 /**
@@ -115,6 +119,115 @@ class FlowableAndroidTest {
         println(values)
     }
 
+    @Test
+    fun test03() {
+        Flowable.just(1).debug("f1").debugWithThread("f1-thread").test().await()
+        Flowable.error<Int>(RuntimeException("test")).debug("f2").debugWithThread("f2-thread").test().assertError(RuntimeException::class.java)
+    }
+
+    @Test
+    fun test04() {
+        val mainThread = Looper.getMainLooper().thread
+        Flowable.just(1).observeMain().doOnNext {
+            Assert.assertEquals(mainThread, Thread.currentThread())
+        }.test().await()
+    }
+
+
+    @Test
+    fun test05() {
+        val currentThread = Thread.currentThread()
+
+        var latch = CountDownLatch(1)
+        val targetThreadName = AtomicReference<String>()
+
+        Schedulers.single().scheduleDirect {
+            targetThreadName.set(Thread.currentThread().name)
+            latch.countDown()
+        }
+
+        latch.await()
+        latch = CountDownLatch(1)
+
+        val result = mutableListOf<String>()
+        val flowable = Flowable.just(1, 2)
+        flowable.subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.single())
+            .autoSubscribe {
+                doOnStart {
+                    println("onStart")
+                    result.add("start")
+                    Assert.assertEquals(currentThread, Thread.currentThread())
+                }
+                doOnNext {
+                    println("onNext")
+                    result.add("next")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                }
+                doOnComplete {
+                    println("onComplete")
+                    result.add("complete")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                    latch.countDown()
+                }
+                doOnError {
+                    println("onError")
+                    result.add("error")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                }
+            }
+
+        flowable.test().await().assertComplete()
+        latch.await()
+        Assert.assertEquals(listOf("start", "next", "next", "complete"), result)
+    }
+
+
+    @Test
+    fun test06() {
+        val currentThread = Thread.currentThread()
+
+        var latch = CountDownLatch(1)
+        val targetThreadName = AtomicReference<String>()
+
+        Schedulers.single().scheduleDirect {
+            targetThreadName.set(Thread.currentThread().name)
+            latch.countDown()
+        }
+
+        latch.await()
+
+        latch = CountDownLatch(1)
+
+        val result = mutableListOf<String>()
+        val flowable = Flowable.error<Int>(RuntimeException("test"))
+        flowable.subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.single())
+            .autoSubscribe(AutoDisposableSubscriber {
+                doOnStart {
+                    result.add("start")
+                    Assert.assertEquals(currentThread, Thread.currentThread())
+                }
+                doOnNext {
+                    result.add("next")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                }
+                doOnComplete {
+                    result.add("complete")
+                    println("${Thread.currentThread()}")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                }
+                doOnError {
+                    result.add("error")
+                    Assert.assertEquals(targetThreadName.get(), Thread.currentThread().name)
+                    latch.countDown()
+                }
+            })
+
+        flowable.test().await().assertError(RuntimeException::class.java)
+        latch.await()
+        Assert.assertEquals(listOf("start", "error"), result)
+    }
 
     interface ITestEvent
     object TestEvent01 : ITestEvent
